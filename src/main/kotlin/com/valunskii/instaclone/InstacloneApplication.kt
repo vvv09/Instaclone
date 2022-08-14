ab4b052522a5d6b52a5af505b852867e327fac6b
Vadim Valunskii <valunski@mail.ru>
2022-08-14 11:38:35 +0300

    Initial Commit: scaffold Spring and Quasar projects

---
+++ src/main/kotlin/com/valunskii/instaclone/InstacloneApplication.kt
@@ -0,0 +1,11 @@
+package com.valunskii.instaclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InstacloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<InstacloneApplication>(*args)
+}
---
+++ src/test/kotlin/com/valunskii/instaclone/InstacloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.valunskii.instaclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InstacloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
