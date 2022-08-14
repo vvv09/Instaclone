f7b51d924bc0433a571ed3d1b47c3932acdf1cfd
Vadim Valunskii <valunski@mail.ru>
2022-08-14 21:20:33 +0300

    Create Frontend: sketch code for deal with camera. WORK NEEDED!

--- frontend/src/pages/Camera.vue
+++ frontend/src/pages/Camera.vue
@@ -5,20 +5,52 @@
     <div
       class="camera-frame q-pa-md"
     >
-      <img
+      <video
+        v-show="!imageCaptured"
+        ref="video"
         class="full-width"
-        src="https://cdn.quasar.dev/img/mountains.jpg"
-      >
+        autoplay
+      />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      />
     </div>
+
     <div
       class="text-center q-pa-md"
     >
       <q-btn
+        v-if="hasCameraSupport"
         size="lg"
         round
         color="grey-10"
         icon="eva-camera"
+        @click="captureImage"
       />
+      <div
+        v-else
+        class="row justify-center q-ma-md"
+      >
+      <q-file
+        dense
+        @input="captureImageFallback"
+        accept="image/*"
+        class="col col-sm-6"
+        label="Chose an Image"
+        v-model="uploadedImage"
+      >
+        <template
+          v-slot:prepend
+        >
+          <q-icon
+            name="eva-attach-outline"
+          />
+        </template>
+      </q-file>
+      </div>
 
       <div
         class="row justify-center q-ma-md"
@@ -69,7 +101,7 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { ref, onMounted, onBeforeUnmount } from 'vue'
   import { uid } from 'quasar'
 
   const post = ref({
@@ -79,6 +111,91 @@
     photo: null,
     date: Date.now() // TODO поудмать над тем чтобы перенесть создание таймштампа на бэкэнде()
   })
+
+  /*
+    Handling Video
+  */
+  require('md-gum-polyfill') //либа нужна для корректного ображения к камера во всех браузерах
+  const video = ref(null)
+  const hasCameraSupport = ref(true)
+  onMounted(() => { initCamera() })
+  const initCamera = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: true
+    }).then(stream => {
+      video.srcObject = stream
+    }).catch(error => {
+      hasCameraSupport.value = false
+    })
+  }
+
+  /*
+   Handling taking the picture
+  */
+  const canvas = ref(null)
+  const imageCaptured = ref(false)
+  const captureImage = () => {
+    let v = video
+    let c = canvas
+    c.width = v.getBoundingClientReact.width
+    c.height = v.getBoundingClientReact.height
+    let context = canvas.getContext('2d')
+    context.drawImage(video, 0, 0, c.width, c.height)
+    imageCaptured.value = true
+    post.value.photo = dataURItoBlob(c.toDataURL())
+    disableCamera()
+  }
+  const dataURItoBlob = (dataURI) => {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    let byteString = atob(dataURI.split(',')[1])
+    // separate out the mime component
+    let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    // write the bytes of the string to an ArrayBuffer
+    let ab = new ArrayBuffer(byteString.length)
+    // create a view into the buffer
+    let ia = new Uint8Array(ab)
+    // set the bytes of the buffer to the correct values
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i)
+    }
+    // write the ArrayBuffer to a blob, and you're done
+    let blob = new Blob([ab], {type: mimeString})
+    return blob
+  }
+
+  /*
+   Handling upload image
+   (when browser can't access camera for some reason)
+  */
+  const uploadedImage = ref([])
+  const captureImageFallback = (file) => {
+    post.value.photo = file
+
+    let c = canvas
+    let context = c.value.getContext('2d')
+
+    let reader = new FileReader()
+    reader.onload = event => {
+      let img = new Image()
+      img.onload = () => {
+        canvas.value.width = img.width
+        canvas.value.height = img.height
+        context.drawImage(img,0,0)
+        imageCaptured.value = true
+      }
+      img.src = event.target.result
+    }
+    reader.readAsDataURL(file) // todo fix - тет валится, что файл не Blob
+  }
+
+  /*
+   Handling disable camera when living the page
+  */
+  onBeforeUnmount(() => { disableCamera() })
+  // todo когда будет камера, убедиться что она отключается после создания снимка или после перехода на другую страницу!
+  const disableCamera = () => { video.srcObject.getAudioTracks().forEach(track => { track.stop() }) }
+
 </script>
 
 
